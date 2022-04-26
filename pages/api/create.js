b20fdc95639c5b5d6eea22d27bf20b5d3bbbf0da
Abby Fischler <abigail.fischler@gmail.com>
2022-04-25 21:04:52 -0700

    🚧 added links to airtable and made basic cards!

---
+++ pages/api/create.js
@@ -0,0 +1,21 @@
+const AirtablePlus = require("airtable-plus")
+
+const airtable = new AirtablePlus({
+    baseID: 'appR7um8HOVQxsYWq',
+    apiKey: process.env.AIRTABLE_API_KEY,
+    tableName: "Table 1"
+})
+
+export default async (req, res) => {
+    if (req.query) {
+        const record = await airtable.create({
+            'Teacher Name': req.query.teachername,
+            'Location': req.query.location,
+            'Bio': req.query.bio,
+            'Link': req.query.wishlist,
+        });
+        res.status(200).send(`Created record ${record.id}. Data inputed was ${JSON.stringify(req.query)}`)
+    } else {
+        res.status(400).send(`Couldn't create record.`)
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -10,36 +10,41 @@ export default function Home() {
 
   const { register, handleSubmit } = useForm();
   const onSubmit = async (d) => {
-    fetch('/api/create?name=' + d.name)
+    fetch(`/api/create?teachername=${d.teachername}&location=${d.location}&bio=${d.bio}&wishlist=${d.wishlist}`)
     .then(r => alert(r.data));
   }
 
 
-  const names = useSWR('/api/all', fetcher).data;
+  const wishlists = useSWR('/api/all', fetcher).data;
   return (
     <div className="dark:text-white dark:bg-black">
       <main>
         <section className="w-1/2 mx-auto bg-gray-200 rounded-xl p-10 mt-16">
           <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-5">
-            <label htmlFor="name">Teacher Name (required)</label>
-            <input className="bg-gray-200 border-b-2 border-gray-400 focus-within:outline-none focus-within:border-gray-600" type="text" id="teacherName" placeholder="Dr. Spell" {...register("name", { required: true })} />
+            <label htmlFor="teacherName">Teacher Name (required)</label>
+            <input className="bg-gray-200 border-b-2 border-gray-400 focus-within:outline-none focus-within:border-gray-600" type="text" id="teachername" placeholder="Dr. Spell" {...register("teachername", { required: true })} />
             <label htmlFor="location">Location</label>
-            <input className="bg-gray-200 border-b-2 border-gray-400 focus-within:outline-none focus-within:border-gray-600" type="text" id="location" placeholder="Los Angeles" {...register("Location", { required: true })} />
-            <label htmlFor="Bio">Bio</label>
-            <input className="bg-gray-200 border-b-2 border-gray-400 focus-within:outline-none focus-within:border-gray-600" type="text" id="bio" placeholder="I am a classroom from XXXXXX" {...register("Location", { required: true })} />
-            <label htmlFor="Bio">Wishlist link</label>
-            <input className="bg-gray-200 border-b-2 border-gray-400 focus-within:outline-none focus-within:border-gray-600" type="text" id="link" placeholder="amazon.com/" {...register("Location", { required: true })} />
+            <input className="bg-gray-200 border-b-2 border-gray-400 focus-within:outline-none focus-within:border-gray-600" type="text" id="location" placeholder="Los Angeles" {...register("location", { required: true })} />
+            <label htmlFor="bio">Bio</label>
+            <input className="bg-gray-200 border-b-2 border-gray-400 focus-within:outline-none focus-within:border-gray-600" type="text" id="bio" placeholder="I am a classroom from XXXXXX" {...register("bio", { required: true })} />
+            <label htmlFor="wishlist">Wishlist link</label>
+            <input className="bg-gray-200 border-b-2 border-gray-400 focus-within:outline-none focus-within:border-gray-600" type="text" id="wishlist" placeholder="amazon.com/" {...register("wishlist", { required: true })} />
             <input type="submit" className="bg-blue-500 text-white py-2 hover:bg-blue-600 rounded-xl font-bold" value="Submit"/>
           </form>
         </section>
-        <section className="flex flex-col bg-gray-200 w-1/2 mx-auto rounded-xl p-10 mt-16">
-          <h1 className="font-bold text-xl mb-2">Names</h1>
+       
+         
           {
-            names?.map(({name}) => (
-              <p className="font-mono">- {name}</p>
+            wishlists?.map(({teacherName, location, bio, link}) => (
+              <div className='bg-black my-3 w-1/4 bg-opacity-50 rounded py-4 px-8'>
+                <p className="font-mono">- {teacherName}</p>
+                <p className="font-mono">- {location}</p>
+                <p className="font-mono">- {bio}</p>
+                <p className="font-mono">- {link}</p>
+              </div>
             ))
           }
-        </section>
+   
       </main>
     </div>
   )}
