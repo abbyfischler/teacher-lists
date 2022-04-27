3805b9f464e9100bb790ba9164d516ddc7dbb16b
Abby Fischler <abigail.fischler@gmail.com>
2022-04-26 20:24:01 -0700

    ✅ Added grid view

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <Nav></Nav>
       <main>
 <section>
-  <p className="text-center m-20 text-base">Schools and school districts don’t always provide enough funding for teachers to provide everything they need for their students. Teachers end up spending their own money on buying supplies for their classrooms. Fortunately, there are people out there who want to help these teachers out. Teachers lists provide these people with a way to help the teachers. This website will store the amazon teacher’s wish list!</p>
+  <p className="text-center my-20 text-base w-2/3 mx-auto">Schools and school districts don’t always provide enough funding for teachers to provide everything they need for their students. Teachers end up spending their own money on buying supplies for their classrooms. Fortunately, there are people out there who want to help these teachers out. Teachers lists provide these people with a way to help the teachers. This website will store the amazon teacher’s wish list!</p>
 </section>
 <section className="bg-gamboge p-10 flex flex-col justify-center">
 <h1 className="text-center text-5xl m-4">Are you a teacher who wants to add your list?</h1>
@@ -22,16 +22,20 @@ export default function Home() {
   <button className="bg-white p-3 rounded-md text-2xl w-fitcontent mx-auto">Make lists!</button>
 </Link>
 </section>
-
+    <section className="grid grid-cols-3 gap-8 mx-16 my-10">
         {wishlists?.map(({ teacherName, location, bio, link }) => (
-          <div className="bg-black my-3 w-1/4 bg-opacity-50 rounded py-4 px-8">
+          <div className="bg-black bg-opacity-50 rounded py-4 px-8">
             <p className="font-mono">- {teacherName}</p>
             <p className="font-mono">- {location}</p>
             <p className="font-mono">- {bio}</p>
             <p className="font-mono">- {link}</p>
           </div>
+        
+
         ))}
+        </section>
       </main>
     </div>
+    
   );
 }
