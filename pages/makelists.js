9988388ff523b30e64d03386bb8123cca4850931
Abby Fischler <abigail.fischler@gmail.com>
2022-04-27 20:24:43 -0700

    ✨ Added error messages for all fields

--- pages/makelists.js
+++ pages/makelists.js
@@ -30,7 +30,7 @@ export default function MakeLists() {
             type="text"
             id="teachername"
             placeholder="Dr. Spell"
-            {...register("teachername", { required: true })}
+            {...register("teacherName", { required: { value: true, message: "You need a teacher name." } })}
           />
           <label htmlFor="location" className="text-gamboge">Location</label>
           <input
@@ -38,7 +38,7 @@ export default function MakeLists() {
             type="text"
             id="location"
             placeholder="Los Angeles"
-            {...register("location", { required: true })}
+            {...register("location", { required: { value: true, message: "You need a location." } })}
           />
           <label htmlFor="bio" className="text-gamboge">Bio</label>
           <input
@@ -46,7 +46,7 @@ export default function MakeLists() {
             type="text"
             id="bio"
             placeholder="I am a classroom from XXXXXX"
-            {...register("bio", { required: true })}
+            {...register("bio", { required: { value: true, message: "You need a bio." } })}
           />
           <label htmlFor="wishlist" className="text-gamboge">Wishlist link</label>
           <input
@@ -64,7 +64,21 @@ export default function MakeLists() {
             name="wishlist"
             render={({ message }) => <p className="text-red-500 font-medium">{message}</p>}
           />
-          {errors?.wishlist?.message}
+          <ErrorMessage
+            errors={errors}
+            name="bio"
+            render={({ message }) => <p className="text-red-500 font-medium">{message}</p>}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="location"
+            render={({ message }) => <p className="text-red-500 font-medium">{message}</p>}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="teacherName"
+            render={({ message }) => <p className="text-red-500 font-medium">{message}</p>}
+          />
           <input
             type="submit"
             className="bg-white text-maroonx-11 hover:bg-maroonx-11 hover:text-white rounded-xl font-bold p-3 w-1/2 mx-auto text-xl"
