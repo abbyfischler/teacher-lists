00a88a4a21019675b2c6c109e0858dc9665f5e92
Abby Fischler <abigail.fischler@gmail.com>
2022-04-28 11:42:46 -0700

    Updated hover!

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
 return(
     <>
-    <h1 className="text-center w-full bg-hpinkk p-3 mt-5">Made with love by <a className="text-white hover:text-blue-400"href="https://abbyfischler.com">Abby Fischler</a></h1>
+    <h1 className="text-center w-full bg-hpinkk p-3 mt-5">Made with love by <a className="text-white hover:text-stone-200"href="https://abbyfischler.com">Abby Fischler</a></h1>
     </>
 )
 }
