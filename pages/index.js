6d7a047e1cc478b92ae5094b93cac23a6497a060
Abby Fischler <abigail.fischler@gmail.com>
2022-04-28 11:38:08 -0700

    Box background to orange gradient

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-16 my-10">
           {wishlists?.map(({ teacherName, location, bio, link, id }) => (
             <div
-              className="flex flex-col bg-regal-blue dark:bg-gamboge rounded-xl py-4 px-8 text-center border border-black dark:text-black"
+              className="flex flex-col bg-orange-grad  dark:bg-gamboge rounded-xl py-4 px-8 text-center border border-black dark:text-black"
               key={id}
             >
               <p className="text-4xl font-bold">{teacherName}</p>
