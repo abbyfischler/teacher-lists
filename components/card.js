5eb1c99a5fe11bd77044458e62613b67344fccbb
Abby Fischler <abigail.fischler@gmail.com>
2022-05-11 22:22:26 -0700

    💄 Added max width to card

--- components/card.js
+++ components/card.js
@@ -12,10 +12,10 @@ export default function Card({ teacherName, location, bio, link, image}) {
                       <p className="text-xl flex">{location}</p>
                   </div>
               </div>
-              <p>{bio}</p>
+              <p className="max-w-md">{bio}</p>
               <a
                   href={link}
-                  className="space-x-1 py-2 text-lg bg-white rounded w-fitcontent mx-auto p-3 m-2 align-center justify-center text-maroonx-11 dark:bg-black dark:text-white"
+                  className="space-x-1 py-2 text-lg bg-white w-full text-center border border-black rounded mx-auto p-3 align-center justify-center text-maroonx-11 dark:bg-black dark:text-white"
               >
                   Link
               </a>
