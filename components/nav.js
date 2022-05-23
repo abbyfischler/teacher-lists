ac388c3ae1e1f6f01fd88216a7413b8c2599ef15
Abby Fischler <abigail.fischler@gmail.com>
2022-05-23 09:29:12 -0700

    🎨 Updated spacing

--- components/nav.js
+++ components/nav.js
@@ -18,44 +18,45 @@ export default function Nav() {
   });
   return (
     <>
-    <Head> 
-    <title>Teacher Lists</title>
-    </Head>
-    <nav className="dark:text-white bg-maroonx-11">
-          
-      <ul className="flex flex-wrap sm:justify-between items-start sm:items-center p-6 mt-4 sm:mt-0">
-        <li>
-          <Link href="/"><img src="/logo.png" className="w-28 h-28"></img></Link>
-          </li>
-        <ul className={`mx-auto sm:mx-0 flex flex-row space-x-5`}>
-          {links.map(({ href, label }) => (
-            <li className="self-center" key={`${href}${label}`}>
-              <Link href={href}>
-                <a
-                  className={`font-inter px-4 py-2 rounded hover:bg-black dark:hover:bg-white hover:bg-opacity-10 dark:hover:bg-opacity-10`}
-                >
-                  {label}
-                </a>
-              </Link>
-            </li>
-          ))}
+      <Head>
+        <title>Teacher Lists</title>
+      </Head>
+      <nav className="dark:text-white bg-maroonx-11">
+        <ul className="flex flex-wrap sm:justify-between items-start sm:items-center p-6 mt-4 sm:mt-0">
           <li>
-            <button
-              onClick={() => {
-                setTheme(theme === "dark" ? "light" : "dark");
-                document
-                  .querySelector("#theme_toggle")
-                  .classList.toggle("rotate-180");
-              }}
-              className="p-2 rounded-full hover:bg-black dark:hover:bg-white hover:bg-opacity-10 dark:hover:bg-opacity-10 transform duration-200"
-              id="theme_toggle"
-            >
-              <CgDarkMode size={24} />
-            </button>
+            <Link href="/">
+              <img src="/logo.png" className="w-28 h-28"></img>
+            </Link>
           </li>
+          <ul className={`mx-auto sm:mx-0 flex flex-row space-x-5`}>
+            {links.map(({ href, label }) => (
+              <li className="self-center" key={`${href}${label}`}>
+                <Link href={href}>
+                  <a
+                    className={`font-inter px-4 py-2 rounded hover:bg-black dark:hover:bg-white hover:bg-opacity-10 dark:hover:bg-opacity-10`}
+                  >
+                    {label}
+                  </a>
+                </Link>
+              </li>
+            ))}
+            <li>
+              <button
+                onClick={() => {
+                  setTheme(theme === "dark" ? "light" : "dark");
+                  document
+                    .querySelector("#theme_toggle")
+                    .classList.toggle("rotate-180");
+                }}
+                className="p-2 rounded-full hover:bg-black dark:hover:bg-white hover:bg-opacity-10 dark:hover:bg-opacity-10 transform duration-200"
+                id="theme_toggle"
+              >
+                <CgDarkMode size={24} />
+              </button>
+            </li>
+          </ul>
         </ul>
-      </ul>
-    </nav>
-  </>
+      </nav>
+    </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,13 @@ import Nav from "../components/nav";
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
-      <div className="flex flex-col min-h-screen" >
-      <Nav />
-      <Component {...pageProps} />
-      <Footer className="self-end" />
+      <div className="flex flex-col min-h-screen">
+        <Nav />
+        <Component {...pageProps} />
+        <Footer className="self-end" />
       </div>
     </ThemeProvider>
   );
 }
 
 export default MyApp;
-
