4d209161934cffdc9dd01f2b1904e0be891ee85e
Abby Fischler <abigail.fischler@gmail.com>
2022-06-24 09:37:08 -0700

    Linked to Github

--- pages/about.js
+++ pages/about.js
@@ -12,8 +12,11 @@ export default function About() {
           teacher’s amazon wish list. I wanted to create a website where
           teachers can post their lists for people to help, so I created this
           site! Please email me with any suggestions or for media:
-          abigail.fischler@gmail.com!
+          abigail.fischler@gmail.com! 
         </p>
+
+     
+        <h3 className="pt-8 mx-8 p-4"><a href="https://github.com/abbyfischler/teacher-lists">This website is open sourced. Click me to see the Github!</a></h3>
       </div>
     </>
   );
